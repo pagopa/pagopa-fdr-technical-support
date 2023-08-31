c120c9d405345a6f2bffce81f6b6559ae079611d
grassog <giovanni.grasso@tasgroup.eu>
2023-08-31 18:22:39 +0200

    created jUnit Test

--- src/test/java/it/gov/pagopa/fdrtechsupport/resources/GetByPspAndIuvTest.java
+++ src/test/java/it/gov/pagopa/fdrtechsupport/resources/GetByPspAndIuvTest.java
@@ -6,10 +6,10 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.common.mapper.TypeRef;
 import it.gov.pagopa.fdrtechsupport.models.FdrBaseInfo;
 import it.gov.pagopa.fdrtechsupport.resources.response.FrResponse;
-
 import it.gov.pagopa.fdrtechsupport.util.AzuriteResource;
 import it.gov.pagopa.fdrtechsupport.util.MongoResource;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
@@ -37,7 +37,8 @@ public class GetByPspAndIuvTest {
                         .then()
                         .statusCode(200)
                         .extract()
-                        .as(new TypeRef<FrResponse>() {});
+                        .as(new TypeRef<>() {
+                        });
 
         List<FdrBaseInfo> data = res.getData();
         assertThat(data.size(), greaterThan(0));
@@ -46,6 +47,41 @@ public class GetByPspAndIuvTest {
         assertThat(data.get(0).getFlowName(), equalTo("string"));
         assertThat(data.get(0).getOrganizationId(), equalTo("string"));
         assertThat(data.get(0).getCreated(), equalTo("2022-07-27T16:15:50Z"));
+    }
 
+    @Test
+    public void testGetFdrByPspAndIuvMalformedError() {
+
+                given()
+                        .param("dateFrom", "27-07-2022")
+                        .param("dateTo", "27-07-2022")
+                        .when()
+                        .get(url.formatted(25, 1))
+                        .then()
+                        .statusCode(equalTo(404));
+    }
+
+    @Test
+    public void testGetFdrByPspAndIuvEmptyParamError() {
+
+        given()
+                .param("dateFrom", "")
+                .param("dateTo", "")
+                .when()
+                .get(url.formatted(25, 1))
+                .then()
+                .statusCode(equalTo(404));
+    }
+
+    @Test
+    public void testGetFdrByPspAndIurReverseDate() {
+
+        given()
+                .param("dateFrom", "27-07-2022")
+                .param("dateTo", "27-05-2022")
+                .when()
+                .get(url.formatted(25, 1))
+                .then()
+                .statusCode(equalTo(404));
     }
 }
