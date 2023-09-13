1ced96d42d5bdf19104626dc66a73995f9bc5730
Cesare Caccuri <cesarecaccuri@gmail.com>
2023-09-13 10:37:06 +0200

    updated by design review

--- src/main/java/it/gov/pagopa/fdrtechsupport/clients/FdrOldRestClient.java
+++ src/main/java/it/gov/pagopa/fdrtechsupport/clients/FdrOldRestClient.java
@@ -1,10 +1,13 @@
 package it.gov.pagopa.fdrtechsupport.clients;
 
+import io.quarkus.rest.client.reactive.ClientExceptionMapper;
 import it.gov.pagopa.fdrtechsupport.clients.model.FdrOldXmlResponse;
+import it.gov.pagopa.fdrtechsupport.exceptions.AppErrorCodeMessageEnum;
+import it.gov.pagopa.fdrtechsupport.exceptions.AppException;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -17,5 +20,21 @@ public interface FdrOldRestClient {
     @Path("/internal/organizations/{ec}/flows/{flowName}")
     FdrOldXmlResponse nodoChiediFlussoRendicontazione(@PathParam("ec") String ec, @PathParam("flowName") String flowName);
 
+    @ClientExceptionMapper
+    static RuntimeException toException(Response response) {
+        if (response.getStatus() == 500) {
+            return new AppException(
+                    AppErrorCodeMessageEnum.ERROR
+            );
+        }
+        if (response.getStatus() == 404) {
+            return new AppException(
+                    AppErrorCodeMessageEnum.FLOW_NOT_FOUND
+            );
+        }
+        return new AppException(
+                AppErrorCodeMessageEnum.ERROR
+        );
+    }
 }
 
