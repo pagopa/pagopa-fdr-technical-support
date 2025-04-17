e8612307d02f4dc2081e7315f8b5696f2d0a68b7
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-04-17 18:26:25 +0200

    [PAGOPA-2696] fix: updating APIs infrastructure

--- infra/99_main.tf
+++ infra/99_main.tf
@@ -26,3 +26,9 @@ provider "azapi" {}
 data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
+
+
+module "__v3__" {
+  # v8.62.1
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3?ref=f3485105e35ce8c801209dcbb4ef72f3d944f0e5"
+}
